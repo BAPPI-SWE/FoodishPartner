14d4ac70b139195879fe1c6fc1c1538cea04a603
Rakibul islam Bappi <bappi616@gmail.com>
2026-07-06 23:50:29 +0600

    kotlin fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
+
+  
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     // ADD THIS NEW PLUGIN
     id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.gms.google-services")
+
+
 }
 
 android {
@@ -31,11 +35,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    alias(libs.plugins.kotlin.compose) apply false
-    id("com.android.application") version "8.10.1" apply false // Or your current version
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false // Or your current version
+  
+
+
+    id("com.android.application") version "8.10.1" apply false
+
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false  // ← add this
     id("com.google.gms.google-services") version "4.4.1" apply false
+    id("com.google.devtools.ksp") version "2.2.20-2.0.4" apply false
 }
